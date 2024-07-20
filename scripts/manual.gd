f767894563a17f725cd540c5df55ee42eb3f9975
Minejas ._. <jafs12@alu.ua.es>
2024-07-20 21:35:11 +0200

    Manual scripts added

--- scripts/manual.gd
+++ scripts/manual.gd
@@ -1,5 +1,9 @@
-extends Sprite2D
+@tool
+class_name manual extends Node2D
+ 
+@export var manual_image: Sprite2D
 
+@onready var base_manual: Sprite2D = $baseManualSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +13,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func highlight():
+	base_manual.set_modulate(Color(0, 1, 1, 1))
+
+func unhighlight():
+	base_manual.set_modulate(Color(1, 1, 1, 1))
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/displayed_manual.tscn")
+	
+func _on_button_mouse_entered():
+	highlight()
+
+func _on_button_mouse_exited():
+	unhighlight()
