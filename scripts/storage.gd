f4c54a0a4b6f93eb8422c58ed6dbf88a0a7223f8
Minejas ._. <jafs12@alu.ua.es>
2024-07-22 14:40:12 +0200

    Escena almacen acabada

--- scripts/storage.gd
+++ scripts/storage.gd
@@ -58,6 +58,9 @@ extends Node2D
 
 var isHighlited = false
 var isLabelShowed = false
+var isBaseSelected = false
+var isEnhancerSelected = false
+var isFlavourSelected = false
 
 func _ready():
 	pass
@@ -88,118 +91,187 @@ func hideLabel(name, type):
 	type.visible = false
 	isLabelShowed = false
 
+#Funcion que resetea todas las bases
+
+func reset_base(type: String):
+	
+	match type:
+		"base":
+	
+			mercurio.play("default")
+			estanyo.play("default")
+			oro.play("default")
+			cobre.play("default")
+			hierro.play("default")
+			isBaseSelected = false
+	
+		"enhancer":
+			
+			litio.play("default")
+			cal.play("default")
+			ladrillo.play("default")
+			magnesio.play("default")
+			oropimente.play("default")
+			isEnhancerSelected = false
+	
+		"flavour":
+			
+			miel.play("default")
+			lupulo.play("default")
+			jengibre.play("default")
+			canela.play("default")
+
+#Cambia el sprite si se selecciona un ingrediente
+
+func select_sprite(image, type: String):
+	
+	match type:
+		"base":
+			
+			if(isBaseSelected):
+				reset_base(type)
+			isBaseSelected = true
+		
+		"enhancer":
+			
+			if(isEnhancerSelected):
+				reset_base(type)
+			isEnhancerSelected = true
+		
+		"flavour":
+			
+			if(isFlavourSelected):
+				reset_base(type)
+			isFlavourSelected = true
+		
+	image.play("marcado")
+
+
 #Signals
+
+func _on_mercurio_button_pressed():
+	select_sprite(mercurio, "base")
 
 func _on_mercurio_button_mouse_entered():
 	highlight(mercurio)
 	showLabel(mercurio_name, mercurio_type)
 
-
 func _on_mercurio_button_mouse_exited():
 	unhighlight(mercurio)
 	hideLabel(mercurio_name, mercurio_type)
-	
+
+func _on_estanyo_button_pressed():
+	select_sprite(estanyo, "base")
 
 func _on_estanyo_button_mouse_entered():
 	highlight(estanyo)
 	showLabel(estanyo_name, estanyo_type)
 
-
 func _on_estanyo_button_mouse_exited():
 	unhighlight(estanyo)
 	hideLabel(estanyo_name, estanyo_type)
 
+func _on_oro_button_pressed():
+	select_sprite(oro, "base")
 
 func _on_oro_button_mouse_entered():
 	highlight(oro)
 	showLabel(oro_name, oro_type)
 
-
 func _on_oro_button_mouse_exited():
 	unhighlight(oro)
 	hideLabel(oro_name, oro_type)
 
+func _on_cobre_button_pressed():
+	select_sprite(cobre, "base")
 
 func _on_cobre_button_mouse_entered():
 	highlight(cobre)
 	showLabel(cobre_name, cobre_type)
 
-
 func _on_cobre_button_mouse_exited():
 	unhighlight(cobre)
 	hideLabel(cobre_name, cobre_type)
 
+func _on_hierro_button_pressed():
+	select_sprite(hierro, "base")
 
 func _on_hierro_button_mouse_entered():
 	highlight(hierro)
 	showLabel(hierro_name, hierro_type)
 
-
 func _on_hierro_button_mouse_exited():
 	unhighlight(hierro)
 	hideLabel(hierro_name, hierro_type)
 
+func _on_litio_button_pressed():
+	select_sprite(litio, "enhancer")
 
 func _on_litio_button_mouse_entered():
 	highlight(litio)
 	showLabel(litio_name, litio_type)
 
-
 func _on_litio_button_mouse_exited():
 	unhighlight(litio)
 	hideLabel(litio_name, litio_type)
 
+func _on_cal_button_pressed():
+	select_sprite(cal, "enhancer")
 
 func _on_cal_button_mouse_entered():
 	highlight(cal)
 	showLabel(cal_name, cal_type)
 
-
 func _on_cal_button_mouse_exited():
 	unhighlight(cal)
 	hideLabel(cal_name, cal_type)
 
+func _on_magnesio_button_pressed():
+	select_sprite(magnesio, "enhancer")
 
 func _on_magnesio_button_mouse_entered():
 	highlight(magnesio)
 	showLabel(magnesio_name, magnesio_type)
 
-
 func _on_magnesio_button_mouse_exited():
 	unhighlight(magnesio)
 	hideLabel(magnesio_name, magnesio_type)
 
+func _on_ladrillo_button_pressed():
+	select_sprite(ladrillo, "enhancer")
 
 func _on_ladrillo_button_mouse_entered():
 	highlight(ladrillo)
 	showLabel(ladrillo_name, ladrillo_type)
 
-
 func _on_ladrillo_button_mouse_exited():
 	unhighlight(ladrillo)
 	hideLabel(ladrillo_name, ladrillo_type)
 
+func _on_oropimente_button_pressed():
+	select_sprite(oropimente, "enhancer")
 
 func _on_oropimente_button_mouse_entered():
 	highlight(oropimente)
 	showLabel(oropimente_name, oropimente_type)
 
-
-
 func _on_oropimente_button_mouse_exited():
 	unhighlight(oropimente)
 	hideLabel(oropimente_name, oropimente_type)
 
+func _on_miel_button_pressed():
+	select_sprite(miel, "flavour")
 
 func _on_miel_button_mouse_entered():
 	highlight(miel)
 	showLabel(miel_name, miel_type)
 
-
 func _on_miel_button_mouse_exited():
 	unhighlight(miel)
 	hideLabel(miel_name, miel_type)
 
+func _on_lupulo_button_pressed():
+	select_sprite(lupulo, "flavour")
 
 func _on_lupulo_button_mouse_entered():
 	highlight(lupulo)
@@ -209,15 +281,19 @@ func _on_lupulo_button_mouse_exited():
 	unhighlight(lupulo)
 	hideLabel(lupulo_name, lupulo_type)
 
+func _on_jengibre_button_pressed():
+	select_sprite(jengibre, "flavour")
 
 func _on_jengibre_button_mouse_entered():
 	highlight(jengibre)
 	showLabel(jengibre_name, jengibre_type)
 
-
 func _on_jengibre_button_mouse_exited():
 	unhighlight(jengibre)
 	hideLabel(jengibre_name, jengibre_type)
+
+func _on_canela_button_pressed():
+		select_sprite(canela, "flavour")
 
 func _on_canela_button_mouse_entered():
 	highlight(canela)
