6a30e66da1166fafb5d80906c37f4ecb8397981b
Daniel Segurado <danielseguradoxd@gmail.com>
2024-07-29 23:10:03 +0200

    Version 1.0 exportable con fullscreen

--- Proyecto/scripts/Options.gd
+++ Proyecto/scripts/Options.gd
@@ -26,3 +26,8 @@ func _on_resolution_item_selected(index):
 		2:
 			DisplayServer.window_set_size(Vector2i(1280,720))
 
+func _on_check_button_toggled(toggled_on):
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
